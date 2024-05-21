ee35766d859ea4d3f5a4539ba8e175aa52fbff7c
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-05-20 22:08:10 -0300

    :sparkes: feat: criando conexão com mongo

--- db/connection.go
+++ db/connection.go
@@ -19,7 +19,7 @@ func GetConnection() (client *mongo.Client, ctx context.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("TODO_DB")))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv(TODO_DB)))
 	if err != nil {
 		log.Fatal(err)
 	}
